5e167c259bdbd5a97c8167e69d91e8f5594e5196
Ali Nabavi <arz1379n@gmail.com>
2022-03-05 20:16:12 +0330

    first commit for adding webRtc library

---
+++ app/src/main/java/com/e/vidihub/activity/NativeVideoCall.kt
@@ -0,0 +1,21 @@
+package com.e.vidihub.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.e.vidihub.R
+import org.webrtc.PeerConnectionFactory
+
+class NativeVideoCall : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_native_video_call)
+
+
+    }
+
+
+    }
+
